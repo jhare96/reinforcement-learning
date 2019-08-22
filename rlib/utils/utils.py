648be4fe7917a330dc3a2f4ae1f223f1985ca52d
jhare96 <jhare1996@hotmail.com>
2019-08-22 09:56:26 +0100

    Running mean std estimate

--- rlib/utils/utils.py
+++ rlib/utils/utils.py
@@ -40,3 +40,35 @@ class rolling_stats(object):
         std = np.sqrt((self.M2 + self.epsilon) / self.n)
         self.mean = new_mean
         return self.mean, std
+
+#https://github.com/openai/baselines/blob/master/baselines/common/running_mean_std.py
+class RunningMeanStd(object):
+    # https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Parallel_algorithm
+    def __init__(self, epsilon=1e-4, shape=()):
+        self.mean = np.zeros(shape, 'float64')
+        self.var = np.ones(shape, 'float64')
+        self.count = epsilon
+
+    def update(self, x):
+        batch_mean = np.mean(x, axis=0)
+        batch_var = np.var(x, axis=0)
+        batch_count = x.shape[0]
+        return self.update_from_moments(batch_mean, batch_var, batch_count)
+
+    def update_from_moments(self, batch_mean, batch_var, batch_count):
+        delta = batch_mean - self.mean
+        tot_count = self.count + batch_count
+
+        new_mean = self.mean + delta * batch_count / tot_count
+        m_a = self.var * (self.count)
+        m_b = batch_var * (batch_count)
+        M2 = m_a + m_b + np.square(delta) * self.count * batch_count / (self.count + batch_count)
+        new_var = M2 / (self.count + batch_count)
+
+        new_count = batch_count + self.count
+
+        self.mean = new_mean
+        self.var = new_var
+        self.count = new_count
+        
+        return self.mean, np.sqrt(self.var)
