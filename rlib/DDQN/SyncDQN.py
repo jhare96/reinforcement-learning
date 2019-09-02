83e7df39a75034cbb0b58bff97a59f08a2eeea69
jhare96 <jhare1996@hotmail.com>
2019-09-02 21:06:01 +0100

    classic control tests plus fixed init

--- rlib/DDQN/SyncDQN.py
+++ rlib/DDQN/SyncDQN.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import numpy as np 
 import gym
 import threading
-import time
+import time, datetime
 from collections import OrderedDict
 
 from rlib.networks.networks import*
@@ -76,13 +76,13 @@ class DQN(object):
 
 
 class SyncDDQN(SyncMultiEnvTrainer):
-    def __init__(self, envs, model, target_model, file_loc, val_envs, action_size,
+    def __init__(self, envs, model, target_model, val_envs, action_size, log_dir='logs/', model_dir='models/',
                      train_mode='nstep', return_type='nstep', total_steps=1000000, nsteps=5, gamma=0.99, lambda_=0.6,
                      validate_freq=1e6, save_freq=0, render_freq=0, update_target_freq=10000, num_val_episodes=50, log_scalars=True, gpu_growth=True,
                      epsilon_start=1, epsilon_final=0.01, epsilon_steps = 1e6, epsilon_test=0.01):
 
         
-        super().__init__(envs=envs, model=model, file_loc=file_loc, val_envs=val_envs, train_mode=train_mode, return_type=return_type, total_steps=total_steps,
+        super().__init__(envs=envs, model=model, val_envs=val_envs, train_mode=train_mode, log_dir=log_dir, model_dir=model_dir, return_type=return_type, total_steps=total_steps,
                 nsteps=nsteps, gamma=gamma, lambda_=lambda_, validate_freq=validate_freq, save_freq=save_freq, render_freq=render_freq,
                 update_target_freq=update_target_freq, num_val_episodes=num_val_episodes, log_scalars=log_scalars, gpu_growth=gpu_growth)
         
@@ -108,7 +108,7 @@ class SyncDDQN(SyncMultiEnvTrainer):
         hyper_paras = OrderedDict(hyper_paras)
 
         if self.log_scalars:
-            filename = file_loc[1] + self.current_time + '/hyperparameters.txt'
+            filename = log_dir + '/hyperparameters.txt'
             self.save_hyperparameters(filename, **hyper_paras)
         
         init = tf.global_variables_initializer()
@@ -192,15 +192,16 @@ def main(env_id):
     num_envs = 32
     nsteps = 20
 
-    train_log_dir = 'logs/SyncDoubleDQN/' + env_id + '/lambda/'
-    model_dir = "models/SyncDoubleDQN/" + env_id + '/'
+    current_time = datetime.datetime.now().strftime('%y-%m-%d_%H-%M-%S')
+    train_log_dir = 'logs/SyncDoubleDQN/' + env_id + '/lambda/' + current_time
+    model_dir = "models/SyncDoubleDQN/" + env_id + '/' + current_time
 
     env = gym.make(env_id)
     
     classic_list = ['MountainCar-v0', 'Acrobot-v1', 'LunarLander-v2', 'CartPole-v0', 'CartPole-v1']
     if any(env_id in s for s in classic_list):
         print('Classic Control')
-        val_envs = [gym.make(env_id) for i in range(10)]
+        val_envs = [gym.make(env_id) for i in range(16)]
         envs = BatchEnv(DummyEnv, env_id, num_envs, blocking=False)
 
     else:
@@ -222,31 +223,32 @@ def main(env_id):
     print('action space', action_size)
 
       
-    Q = DQN(nature_cnn, input_shape=input_size, action_size=action_size, name='Q', lr=1e-3, lr_final=1e-3, grad_clip=0.5, decay_steps=50e6)
-    TargetQ = DQN(nature_cnn, input_shape=input_size, action_size=action_size, name='QTarget', lr=1e-3, lr_final=1e-3, grad_clip=0.5, decay_steps=50e6)  
+    Q = DQN(mlp, input_shape=input_size, action_size=action_size, name='Q', lr=1e-3, lr_final=1e-3, grad_clip=0.5, decay_steps=50e6)
+    TargetQ = DQN(mlp, input_shape=input_size, action_size=action_size, name='QTarget', lr=1e-3, lr_final=1e-3, grad_clip=0.5, decay_steps=50e6)  
 
     
 
     DDQN = SyncDDQN(envs=envs,
                     model=Q,
                     target_model=TargetQ,
-                    file_loc=[model_dir, train_log_dir],
+                    model_dir = model_dir,
+                    log_dir = train_log_dir,
                     val_envs=val_envs,
                     action_size=action_size,
                     train_mode='nstep',
                     return_type='lambda',
-                    total_steps=50e6,
+                    total_steps=2e6,
                     nsteps=nsteps,
                     gamma=0.99,
                     lambda_=0.95,
                     save_freq=0,
                     render_freq=0,
-                    validate_freq=1e6,
+                    validate_freq=4e4,
                     num_val_episodes=50,
                     update_target_freq=10000,
                     epsilon_start=1,
                     epsilon_final=0.01,
-                    epsilon_steps=2e6,
+                    epsilon_steps=8e4,
                     epsilon_test=0.01,
                     log_scalars=True)
     
@@ -263,8 +265,8 @@ def main(env_id):
 if __name__ == "__main__":
     #env_id_list = [ 'SpaceInvadersDeterministic-v4', 'FreewayDeterministic-v4',]# 'MontezumaRevengeDeterministic-v4', ]
     env_id_list = ['MontezumaRevengeDeterministic-v4']
-    #env_id_list = ['MountainCar-v0', 'Acrobot-v1', ]
-    for i in range(1):
+    env_id_list = ['CartPole-v1', 'Acrobot-v1',  'MountainCar-v0',]
+    for i in range(5):
         for env_id in env_id_list:
             main(env_id)
-   # env_id_list = ['MountainCar-v0', 'Acrobot-v1', 'CartPole-v0']
+   # 
